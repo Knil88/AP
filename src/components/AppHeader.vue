64ac1ffcbb74023d11f4517ce2a4bd967f6bbe4b
Yari <narducciyarigmail.com>
2023-01-27 19:16:23 +0100

    creazione messaggio alert

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,18 @@
 <script >
 
-
+export default{
+  data() {
+    return {
+      
+    }
+  
+  },
+  methods: {
+    notReadyYet(){
+      alert('Siamo spiacenti , ma questa parte non è ancora stata ultimata')
+    }
+  },
+}
 
 
 </script>
@@ -38,7 +50,7 @@
           </li>
         </ul>
         
-            <a href="#" id="reserved_link">
+            <a href="#" id="reserved_link" @click="notReadyYet">
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-file-person" viewBox="0 0 16 16">
                 <path d="M12 1a1 1 0 0 1 1 1v10.755S12 11 8 11s-5 1.755-5 1.755V2a1 1 0 0 1 1-1h8zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4z"/>
                 <path d="M8 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
